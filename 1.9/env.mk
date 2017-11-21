44025abdf907427a137bbce9a28e3e4e7c5ce6ca
Thibault Billon <tbillon@online.net>
2017-11-21 14:49:02 +0000

    Bump to golang 1.9 & to the new build system

---
+++ 1.9/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = golang
+IMAGE_VERSION = stable-1.9
+IMAGE_VERSION_ALIASES = 1.9
+IMAGE_TITLE = Golang 1.9
+IMAGE_DESCRIPTION = The Go Programming Language
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-golang
+IMAGE_VENDOR_URL = https://golang.org/
+IMAGE_BOOTSCRIPT = mainline 4.4
